f342a3744596a668d02d664905a28cdb812d870c
sagar kumar <sagarpradhan0@gmail.com>
2022-12-31 19:05:46 +0530

    second time added into repo

--- src/main/java/com/gitDemoApp/gitdemo/App.java
+++ src/main/java/com/gitDemoApp/gitdemo/App.java
@@ -3,6 +3,9 @@ package com.gitDemoApp.gitdemo;
 public class App {
 	public void addData(String data) {
 		System.out.println("Data Is :"+data);
+		if(data.equals("sagar")) {
+			System.out.println("welcome Sagar");
+		}
 	}
   public static void main(String[] args) {
     System.out.println("Hello World!");
