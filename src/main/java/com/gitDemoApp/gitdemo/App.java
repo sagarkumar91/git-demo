4e0d1349e6bbabd8376ffaa238dca31074b6f29e
unknown <sagarprathan0@gmail.com>
2022-12-31 18:55:24 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/gitDemoApp/gitdemo/App.java
@@ -0,0 +1,10 @@
+package com.gitDemoApp.gitdemo;
+
+public class App {
+	public void addData(String data) {
+		System.out.println("Data Is :"+data);
+	}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
